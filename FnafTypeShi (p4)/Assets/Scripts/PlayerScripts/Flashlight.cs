983a475af275c172b418ec559c5812c64506ce23
facts_programmer <viktoreshuis5@gmail.com>
2026-05-18 13:39:21 +0200

    flashlight update
    flashlight flickers

--- FnafTypeShi (p4)/Assets/Scripts/PlayerScripts/Flashlight.cs
+++ FnafTypeShi (p4)/Assets/Scripts/PlayerScripts/Flashlight.cs
@@ -1,34 +1,99 @@
 using UnityEngine;
 
+using System.Collections;
+using UnityEngine;
+
 public class Flashlight : MonoBehaviour
 {
-
+    [Header("Flashlight Objects")]
     public GameObject ON;
     public GameObject OFF;
+
+    [Header("Flicker Settings")]
+    public bool enableFlicker = true;
+   
+    public Vector2 timeBetweenBursts = new Vector2(2f, 6f);
+   
+    public Vector2 burstDuration = new Vector2(0.2f, 1.0f);
+  
+    public Vector2 intraFlickerDelay = new Vector2(0.03f, 0.15f);
+   
+    [Range(0f, 1f)] public float hardCutChance = 0.25f;
+
     private bool isOn = false;
+    private Coroutine flickerRoutine;
+
     void Start()
     {
-        ON.SetActive(false);
-        OFF.SetActive(true);
-        isOn = false;
+        SetFlashlight(false);
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
-            if (isOn)
-                {
-                ON.SetActive(false);
-                OFF.SetActive(true);
-                isOn = false;
+            SetFlashlight(!isOn);
+        }
+    }
+
+    private void SetFlashlight(bool turnOn)
+    {
+        isOn = turnOn;
+        ON.SetActive(isOn);
+        OFF.SetActive(!isOn);
+
+        if (isOn && enableFlicker)
+        {
+            if (flickerRoutine == null)
+                flickerRoutine = StartCoroutine(FlickerLoop());
+        }
+        else
+        {
+            if (flickerRoutine != null)
+            {
+                StopCoroutine(flickerRoutine);
+                flickerRoutine = null;
             }
-            else
+            ON.SetActive(isOn);
+            OFF.SetActive(!isOn);
+        }
+    }
+
+    private IEnumerator FlickerLoop()
+    {
+        var waitBetweenBursts = new WaitForSeconds(0f);
+        while (isOn)
+        {
+            float waitTime = Random.Range(timeBetweenBursts.x, timeBetweenBursts.y);
+            waitBetweenBursts = new WaitForSeconds(waitTime);
+            yield return waitBetweenBursts;
+
+            float endTime = Time.time + Random.Range(burstDuration.x, burstDuration.y);
+            bool lastState = true;
+
+            while (Time.time < endTime && isOn)
+            {
+                bool hardCut = Random.value < hardCutChance;
+
+                // Toggle visibility
+                lastState = !lastState;
+                ON.SetActive(lastState && !hardCut);
+                OFF.SetActive(!(lastState && !hardCut));
+
+                float d = Random.Range(intraFlickerDelay.x, intraFlickerDelay.y);
+                yield return new WaitForSeconds(d);
+
+                if (Random.value < 0.2f)
+                    yield return new WaitForSeconds(Random.Range(0.01f, 0.05f));
+            }
+
+            if (isOn)
             {
                 ON.SetActive(true);
                 OFF.SetActive(false);
-                isOn = true;
             }
         }
+
+        flickerRoutine = null;
     }
 }
